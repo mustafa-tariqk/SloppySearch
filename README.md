70e0b7ea51e51c0be50ca1705f24c5907ead7707
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-02-28 14:13:39 -0500

    README added

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sloppy Search
+Sloppily search through your vectors! Aiming to be quick.
--- main.py
+++ main.py
@@ -14,4 +14,4 @@ class Database:
     def search(self, query_vector, k=10):
         distances = np.linalg.norm(np.array(self.vectors) - query_vector.data, axis=1)
         top_k_indices = np.argsort(distances)[:k]
-        return distances[top_k_indices], top_k_indices 
+        return distances[top_k_indices], top_k_indices
