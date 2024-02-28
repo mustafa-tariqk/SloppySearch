acb6e67b61c226771d16fc6e1d057dc5fc7d9858
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-02-28 15:37:18 -0500

    removing print statements

--- src/sloppy_search/__init__.py
+++ src/sloppy_search/__init__.py
@@ -10,10 +10,8 @@ class Database:
 
     def add_vector(self, vector):
         self.vectors.append(vector)
-        print(f'Vector added: {vector.data}')  # Print statement added
 
     def search(self, query_vector, k=10):
         distances = np.linalg.norm(np.array([v.data for v in self.vectors]) - query_vector.data, axis=1)
         top_k_indices = np.argsort(distances)[:k]
-        print(f'Top {k} indices: {top_k_indices}')
         return distances[top_k_indices], top_k_indices
